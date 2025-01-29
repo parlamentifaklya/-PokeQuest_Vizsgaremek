9473f4dc987ef4b5f451a2af043ce6c1db57fba5
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2025-01-29 11:14:58 +0100

    forgot to update lol

---
+++ backend/PokeQuestApi_New/PokeQuestApi_New/Migrations/20250129101350_sqlite.local_migration_334.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokeQuestApi_New.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlitelocal_migration_334 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
