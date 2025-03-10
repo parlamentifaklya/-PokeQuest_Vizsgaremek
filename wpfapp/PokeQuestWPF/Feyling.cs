00f907a58a116650862002cfe1792fddd22fc610
Sushkoo <szilagyidominik005@gmail.com>
2025-03-10 14:26:57 +0100

    Images now show up, small design change

---
+++ wpfapp/PokeQuestWPF/Feyling.cs
@@ -0,0 +1,16 @@
+﻿public class Feyling
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Img { get; set; }
+    public int TypeId { get; set; }
+    public int AbilityId { get; set; }
+    public bool IsUnlocked { get; set; }
+    public int Hp { get; set; }
+    public int Atk { get; set; }
+    public int? ItemId { get; set; }
+    public int WeakAgainstId { get; set; }
+    public int StrongAgainstId { get; set; }
+    public int SellPrice { get; set; }
+}
---
+++ wpfapp/PokeQuestWPF/ImagePathConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace PokeQuestWPF
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        private const string BaseUrl = "http://localhost:5130/api/";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value == null)
+                return null;
+
+            string relativePath = value.ToString();
+            string fullImageUrl = BaseUrl  +  relativePath.Replace("\\", "/");
+
+            return new BitmapImage(new Uri(fullImageUrl));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null; // Not used in this scenario
+        }
+    }
+}
