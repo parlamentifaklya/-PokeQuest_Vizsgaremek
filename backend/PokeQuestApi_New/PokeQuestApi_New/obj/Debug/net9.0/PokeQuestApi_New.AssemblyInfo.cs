5bd211167d485b073ce625ffdb3110281fc5949c
Kökényesi Andor <20.kokenyesi.andor@mechwart.com>
2025-03-24 10:25:30 +0100

    game is finshed? finally?

--- backend/PokeQuestApi_New/PokeQuestApi_New/obj/Debug/net9.0/PokeQuestApi_New.AssemblyInfo.cs
+++ backend/PokeQuestApi_New/PokeQuestApi_New/obj/Debug/net9.0/PokeQuestApi_New.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+580106c879ec1ebe95ce5b221ad13ecebebea28f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c46936ff539a8d04089d2dec53b06588afa68a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
