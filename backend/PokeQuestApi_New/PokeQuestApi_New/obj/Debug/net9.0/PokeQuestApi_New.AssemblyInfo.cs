097c88e433e9aebd60b69d0246bf5c22331c9b93
Sushkoo <szilagyidominik005@gmail.com>
2025-03-26 09:26:53 +0100

    troubleshooting and layout in the documentation

--- backend/PokeQuestApi_New/PokeQuestApi_New/obj/Debug/net9.0/PokeQuestApi_New.AssemblyInfo.cs
+++ backend/PokeQuestApi_New/PokeQuestApi_New/obj/Debug/net9.0/PokeQuestApi_New.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c46936ff539a8d04089d2dec53b06588afa68a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a7401857200ba951abd321a96ec6f184e43e954")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
