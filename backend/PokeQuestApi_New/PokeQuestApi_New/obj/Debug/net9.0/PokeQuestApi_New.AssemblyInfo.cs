93476d098d195e55d84545a0c8f4edb3b67a3baf
Kökényesi Andor <20.kokenyesi.andor@mechwart.com>
2025-03-27 13:30:59 +0100

    asd

--- backend/PokeQuestApi_New/PokeQuestApi_New/obj/Debug/net9.0/PokeQuestApi_New.AssemblyInfo.cs
+++ backend/PokeQuestApi_New/PokeQuestApi_New/obj/Debug/net9.0/PokeQuestApi_New.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e526d57160b9064de47213f13b10858742ad56e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8915f003487d1994c86371ae9b88a4ec00083f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeQuestApi_New")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
