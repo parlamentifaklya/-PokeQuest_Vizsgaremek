e376b305f832cd1bc1ad253e7d3dbda3665e0b56
Sushkoo <szilagyidominik005@gmail.com>
2025-03-27 11:22:10 +0100

    Documentation-Backend: Fully done

--- PokeQuestDocs/app/devdoc/backend/controller/_meta.js
+++ PokeQuestDocs/app/devdoc/backend/controller/_meta.js
@@ -1,5 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export default{
-
+    abilitycontroller: "KépességKezelő 💪",
+    feylingcontroller: "FeylingKezelő 🦊",
+    itemcontroller: "TárgyKezelő 🗡",
+    typecontroller: "TypeKezelő 🏷️",
+    usercontroller: "FelhasználóKezelő 👤",
 };
