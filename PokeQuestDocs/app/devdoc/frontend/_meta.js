37d0b644f81dceb6821f002d24a7111bd7179e65
Sushkoo <szilagyidominik005@gmail.com>
2025-03-28 08:28:48 +0100

    Documentation-frontend: Services, Modules, Context done

---
+++ PokeQuestDocs/app/devdoc/frontend/_meta.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default{
+    components: "Komponensek",
+    context: "Kontextus",
+    modules: "Modulok ",
+    services: "Szolgáltatások ",
+    types: "Típusok ",
+};
